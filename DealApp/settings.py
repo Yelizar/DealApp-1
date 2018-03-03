4b6281e89c24d514ead6ca4eb21d5e80047756d3
Ilya <alilyabor@gmail.com>
2018-03-03 21:34:04 +1300

    asd

--- DealApp/settings.py
+++ DealApp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base',
+    'access',
 
 ]
 
@@ -120,4 +121,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, "static", "dev"),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static", "static_root")
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- base/models.py
+++ base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+
+    name = models.CharField(max_length=128, default=None, blank=False)
+    price = models.FloatField(default=0, blank=False)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
