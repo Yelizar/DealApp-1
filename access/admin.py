1e4f4133c589609b56e787a6d71dccd5792992e4
Ilya <alilyabor@gmail.com>
2018-03-04 21:10:38 +1300

    asd

--- access/admin.py
+++ access/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+class BuyerProfileAdmin(admin.ModelAdmin):
+
+    list_display = [field.name for field in BuyerProfile._meta.fields]
+
+
+admin.site.register(BuyerProfile, BuyerProfileAdmin)
--- access/models.py
+++ access/models.py
@@ -3,14 +3,26 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class UserProfile(models.Model):
+class BuyerProfile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     photo = models.ImageField('Profile photo', upload_to='access/profile_photo')
     phone = models.CharField('Phone', default=None, blank=True, max_length=128)
-    user_type = models.CharField('User type', default=None, blank=False, max_length=128)
+    map_location = models.CharField('location', default=None, blank=True, max_length=256)
+    is_active = models.BooleanField(default=True)
+
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    class Meta:
+        ordering = ['created']
+        verbose_name = 'User\'s profile'
+        verbose_name_plural = 'User\'s profiles'
 
     def __str__(self):
         return '{} {}'.format(self.user.first_name, self.user.last_name)
+
+
+
 
 
