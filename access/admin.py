c764158a9aacd497fb4b8d68bd4de24267cffbdc
Yelizar <33081804+Yelizar@users.noreply.github.com>
2018-03-05 16:45:11 +1300

    All auth

--- access/admin.py
+++ access/admin.py
@@ -3,9 +3,20 @@ from .models import *
 # Register your models here.
 
 
-# class BuyerProfileAdmin(admin.ModelAdmin):
-#
-#     list_display = [field.name for field in BuyerProfile._meta.fields]
-#
-#
-# admin.site.register(BuyerProfile, BuyerProfileAdmin)
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+
+class BuyerProfileAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in BuyerProfile._meta.fields]
+
+
+admin.site.register(BuyerProfile, BuyerProfileAdmin)
+
+
+class BuyerAddressAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Address._meta.fields]
+
+
+admin.site.register(Address, BuyerAddressAdmin)
--- access/urls.py
+++ access/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'access'
 urlpatterns = [
-    #path("/login/", views.login, name="account_login"),
+    path("login/", views.LoginView.as_view(), name="account_login"),
 ]
