5805030380b48041fbc6585db1f0bb83143652c6
Yelizar <Yelizar.Huryn@gmail.com>
2018-03-05 15:38:23 +1300

    DealApp align centre

--- access/models.py
+++ access/models.py
@@ -1,26 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import User
+
+# from django.contrib.auth.models import User
 # Create your models here.
 
 
-class BuyerProfile(models.Model):
+# class BuyerProfile(models.Model):
+#
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     photo = models.ImageField('Profile photo', upload_to='access/profile_photo')
+#     phone = models.CharField('Phone', default=None, blank=True, max_length=128)
+#     map_location = models.CharField('location', default=None, blank=True, max_length=256)
+#     is_active = models.BooleanField(default=True)
+#
+#     created = models.DateTimeField(auto_now=False, auto_now_add=True)
+#     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+#
+#     class Meta:
+#         ordering = ['created']
+#         verbose_name = 'User\'s profile'
+#         verbose_name_plural = 'User\'s profiles'
+#
+#     def __str__(self):
+#         return '{} {}'.format(self.user.first_name, self.user.last_name)
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    photo = models.ImageField('Profile photo', upload_to='access/profile_photo')
-    phone = models.CharField('Phone', default=None, blank=True, max_length=128)
-    map_location = models.CharField('location', default=None, blank=True, max_length=256)
-    is_active = models.BooleanField(default=True)
-
-    created = models.DateTimeField(auto_now=False, auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
-
-    class Meta:
-        ordering = ['created']
-        verbose_name = 'User\'s profile'
-        verbose_name_plural = 'User\'s profiles'
-
-    def __str__(self):
-        return '{} {}'.format(self.user.first_name, self.user.last_name)
 
 
 
--- access/views.py
+++ access/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import View
 # Create your views here.
+from allauth.account.views import LoginView
+
+#class LoginView(LoginView):
+
 
 
