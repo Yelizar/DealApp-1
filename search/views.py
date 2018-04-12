2ebff658c19c6657751a258c0d95ca1f360b6973
Ilya <alilyabor@gmail.com>
2018-04-13 11:56:07 +1200

    Rabotaet blyat

--- search/views.py
+++ search/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from django.views.generic import View
 from access.models import UserProfile
+from suppliers.models import Goods
 from django.db.models import Q
 # Create your views here.
 
@@ -9,7 +10,8 @@ def EntireSearch(request):
     template = 'search/search.html'
 
     query = request.GET.get('q')
-    search_users = UserProfile.objects.filter(username__icontains=query)
+    search_users = UserProfile.objects.filter(username__icontains=query)[0:4]
+    search_products = Goods.objects.filter(name__icontains=query)[0:4]
     return render(request, template, locals())
 
 
--- suppliers/models.py
+++ suppliers/models.py
@@ -6,7 +6,7 @@ from access.models import UserProfile
 
 class Goods(models.Model):
     supplier = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    name = models.CharField('Name', max_length=64, default=None, blank=True)
+    name = models.CharField(max_length=64, default=None, blank=True)
     price = models.FloatField('Price', default=0)
     picture = models.ImageField('Picture', upload_to='suppliers/goods', blank=True)
     description = models.TextField('Description', max_length=2048, default=None, blank=True)
