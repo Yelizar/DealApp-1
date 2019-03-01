bcab90109a63025bd30c8a1475a022c84cc467cc
laz <Yelizar.Huryn@gmail.com>
2019-03-01 16:54:43 +1300

    new design List of products supplier side

--- static/dev/js/chat.js
+++ static/dev/js/chat.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
            $.ajax({
                url: "http://127.0.0.1:8000/chat/",
                success: function(dat){
-                    $('#mes').replaceWith("<p class='mes' id='mes'>"+dat+"</p>");
+                    $('#mes').replaceWith("<i id=mes'>"+dat+"</i>");
         }
         });
     }, 2000);
