4588ea9f4add167d45f78caae172a2c6ad370ee4
laz <Yelizar.Huryn@gmail.com>
2019-03-25 20:19:13 +1300

    new design List of products supplier side

--- goods/views.py
+++ goods/views.py
@@ -20,6 +20,39 @@ class ProductDetailView(View):
     template_name = 'goods/product_detail.html'
     feedbacks = []
 
+    # def get(self, request, product_id):
+    #     form = GoodsFeedbackForm()
+    #     feedbacks = []
+    #     try:
+    #         product = Goods.objects.get(id=product_id)
+    #         feedback = GoodsFeedback.objects.filter(product=product, comment=None)
+    #         for feed in feedback:
+    #             self.feedbacks.append(feed)
+    #             self.feedloop(feed)
+    #             feedbacks += self.feedbacks
+    #             self.feedbacks.clear()
+    #     except GoodsFeedback.DoesNotExist:
+    #         feedback = None
+    #     return render(request, self.template_name, locals())
+    # #
+    # # def feedloop(self, feed):
+    # #     variable = 'comment'
+    # #     feedback_replay = GoodsFeedback.objects.filter(**{variable: feed})
+    # #     if feedback_replay:
+    # #         variable += '__comment'
+    # #         self.feedloop(feedback_replay)
+    #
+    # def feed_appender(self, feedback):
+    #     for feed in feedback:
+    #         self.feedbacks.append(feed)
+    #         self.feedloop(feed)
+    #
+    # #     return self.feedbacks
+    # def feedloop(self, feed):
+    #     feedback_replay = GoodsFeedback.objects.filter(comment=feed)
+    #     if feedback_replay:
+    #         self.feed_appender(feedback_replay)
+
     def get(self, request, product_id):
         form = GoodsFeedbackForm()
         feedbacks = []
@@ -27,31 +60,13 @@ class ProductDetailView(View):
             product = Goods.objects.get(id=product_id)
             feedback = GoodsFeedback.objects.filter(product=product, comment=None)
             for feed in feedback:
-                self.feedbacks.append(feed)
-                self.feedloop(feed)
-                feedbacks += self.feedbacks
-                self.feedbacks.clear()
+                feedbacks.append(feed)
+                branch = GoodsFeedback.objects.filter(product=product, branch=feed.branch)
+                feedbacks.append(branch)
+                print(feedbacks)
         except GoodsFeedback.DoesNotExist:
-            feedback = None
+            print(123)
         return render(request, self.template_name, locals())
-    #
-    # def feedloop(self, feed):
-    #     variable = 'comment'
-    #     feedback_replay = GoodsFeedback.objects.filter(**{variable: feed})
-    #     if feedback_replay:
-    #         variable += '__comment'
-    #         self.feedloop(feedback_replay)
-
-    def feed_appender(self, feedback):
-        for feed in feedback:
-            self.feedbacks.append(feed)
-            self.feedloop(feed)
-
-    #     return self.feedbacks
-    def feedloop(self, feed):
-        feedback_replay = GoodsFeedback.objects.filter(comment=feed)
-        if feedback_replay:
-            self.feed_appender(feedback_replay)
 
     def post(self, request, product_id, **kwargs):
         form = GoodsFeedbackForm(data=request.POST)
@@ -62,7 +77,7 @@ class ProductDetailView(View):
             feedback.product_id = product_id
             feedback.writer = request.user
             try:
-                replay = GoodsFeedback.objects.get(comment__comment_id=request.POST['replay-comment-id'])
+                replay = GoodsFeedback.objects.get(id=request.POST['replay-comment-id'])
                 feedback.comment = replay
             except ValueError:
                 feedback.comment = None
