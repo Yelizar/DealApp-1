cb10c92ca762c04f22f5cf5d7630ea037355d011
Yelizar <33081804+Yelizar@users.noreply.github.com>
2020-02-28 18:02:56 +0300

    Update settings.py

--- DealApp/settings.py
+++ DealApp/settings.py
@@ -58,7 +58,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'Yelizar.Huryn@gmail.com'
-EMAIL_HOST_PASSWORD = 'azazelo74100147'
+EMAIL_HOST_PASSWORD = 'password'
 
 
 AUTHENTICATION_BACKENDS = (
